a2fb3cda7269bf3a0ba5e675cea3719241055ae3
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2022-04-23 21:24:28 +0200

    auto push

--- pages/seguridad.py
+++ pages/seguridad.py
@@ -1,5 +1,36 @@
 import streamlit as st
+import pandas as pd
+import json
+from streamlit_folium import folium_static
+import folium
+import numpy as np
 
 
 def seguridad():
-    st.write("test")
+
+    # name = "Alcance.csv"
+    # name = "Colisión frontal.csv"
+    # name = "Vuelco.csv"
+    # name = "Atropello a animal.csv"
+    name = "Despeñamiento.csv"
+    df = pd.read_csv(
+        "output/accidentes/{}".format(name), header="infer", sep=";", encoding="UTF-8"
+    )
+    st.write(df)
+
+    with open("madrid_distritos.json") as f:
+        madrid_distritos = json.load(f)
+    m = folium.Map(location=[40.42, -3.70], zoom_start=10.45)
+    folium.Choropleth(
+        geo_data=madrid_distritos,
+        # topojson="objects.almeria_wm",
+        name="choropleth",
+        data=df,
+        columns=["cod_distrito", "Cantidad"],
+        key_on="feature.properties.cartodb_id",
+        fill_color="RdYlGn_r",
+        fill_opacity=0.5,
+        line_opacity=0.5,
+        legend_name="otra",
+    ).add_to(m)
+    folium_static(m)
