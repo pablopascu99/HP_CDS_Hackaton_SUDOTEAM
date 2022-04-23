2b787b196cd42352a5a7e5bc8a44c53a4ac83c87
N3oZ3r0 <21806108@live.uem.es>
2022-04-23 19:58:33 +0200

    Revive el menu

--- frontend.py
+++ frontend.py
@@ -14,9 +14,7 @@ st.set_page_config(
 
 hide_st_style = """
             <style>
-            #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
