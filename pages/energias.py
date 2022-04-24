20135cc1620eda4b2b3bb4578ca408b880ab8313
N3oZ3r0 <21806108@live.uem.es>
2022-04-24 09:56:07 +0200

    arreglos output

--- pages/energias.py
+++ pages/energias.py
@@ -6,7 +6,7 @@ import folium
 
 def paneles():
     name = "Anthem_CTC_InstalacionesFotovoltaicas2.csv"
-    df = pd.read_csv("data/{}".format(name), header="infer", sep=";", encoding="latin1")
+    df = pd.read_csv("output/{}".format(name), header="infer", sep=";", encoding="latin1")
     # st.write(df)
 
     m = folium.Map(location=[40.42, -3.70], zoom_start=11)
@@ -14,10 +14,10 @@ def paneles():
     for i in range(0, len(df)):
         folium.Marker(
             location=[df.iloc[i]["Latitud"], df.iloc[i]["Longitud"]],
-            # icon=folium.DivIcon(
-            #     html=f"""
-            # <div><img src="https://pngimg.com/uploads/solar_panel/small/solar_panel_PNG106.png" width=50vw height=50vw>
-            #     </div>"""
-            # ),
+            icon=folium.DivIcon(
+                html=f"""
+            <div><img src="https://pngimg.com/uploads/solar_panel/small/solar_panel_PNG106.png" width=50vw height=50vw>
+                </div>"""
+            ),
         ).add_to(m)
     folium_static(m)
--- pages/trafico.py
+++ pages/trafico.py
@@ -8,18 +8,18 @@ import json
 def trafico():
 
     name = "Anthem_CTC_Taxi_ReservaParadas.csv"
-    df = pd.read_csv("data/{}".format(name), header="infer", sep=";", encoding="UTF-8")
+    df = pd.read_csv("output/{}".format(name), header="infer", sep=";", encoding="UTF-8")
 
     m = folium.Map(location=[40.42, -3.70], zoom_start=11)
 
     for i in range(0, len(df)):
         folium.Marker(
             location=[df.iloc[i]["Latitud"], df.iloc[i]["Longitud"]],
-            # icon=folium.DivIcon(
-            #     html=f"""
-            # <div><img src="https://www.pngrepo.com/png/48299/512/taxi.png" width="25" height="25">
-            #     </div>"""
-            # ),
+            icon=folium.DivIcon(
+                html=f"""
+            <div><img src="https://www.pngrepo.com/png/48299/512/taxi.png" width="25" height="25">
+                </div>"""
+            ),
         ).add_to(m)
 
     with open("madrid_barrios.json", encoding="UTF-8") as f:
