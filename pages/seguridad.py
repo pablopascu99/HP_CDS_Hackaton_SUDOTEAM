d6db845d790e656597f3b8b703156167a2e6879a
pablopascu99 <pablopascu99@gmail.com>
2022-04-24 13:23:35 +0200

    c

--- pages/seguridad.py
+++ pages/seguridad.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 from streamlit_folium import folium_static
 import folium
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -24,6 +25,30 @@ def seguridad():
         ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
         st.pyplot(fig1)
+
+    
+    name = "qt_plazas.csv"
+    df = pd.read_csv(
+        "output/{}".format(name), header="infer", sep=";", encoding="UTF-8"
+    )
+
+    with open("madrid_distritos.json", encoding="UTF-8") as f:
+        madrid_distritos = json.load(f)
+
+    m = folium.Map(location=[40.42, -3.70], zoom_start=10.45)
+    folium.Choropleth(
+        # topojson="objects.almeria_wm",
+        geo_data=madrid_distritos,
+        name="choropleth",
+        data=df,
+        columns=["DISTRITO", "QT_PLAZAS"],
+        key_on="feature.properties.name",
+        fill_color="RdYlGn_r",
+        fill_opacity=0.5,
+        line_opacity=0.5,
+        legend_name="otra",
+    ).add_to(m)
+    folium_static(m)
 
     name = "accidentesTipo.csv"
     df = pd.read_csv(
